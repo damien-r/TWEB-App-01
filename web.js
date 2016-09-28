e646235110eddc9c4f537a33dd4a35606afbd2b4
Sébastien Richoz <sebastien.richoz1@heig-vd.ch>
2016-09-29 00:08:09 +0200

    removing procfile

--- web.js
+++
@@ -1,8 +0,0 @@
-var gzippo = require('gzippo');
-var express = require('express');
-var app = express();
-
-var logger = require('morgan');
-app.use(logger); //replaces your app.use(express.logger());
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 4000);
